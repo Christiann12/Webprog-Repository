fe1436f7bcc8b7bb675228f9826e80193cd26249
Christiann12 <64347274+Christiann12@users.noreply.github.com>
2021-02-12 17:55:27 +0800

    feat: Added CSS and JS Framework
    Added CSS and JS Framework from bootstrap and edited header, footer, and main view PHP file to test if stylesheets are working.

--- application/views/MainView.php
+++ application/views/MainView.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <!-- About Section -->
   <div class="w3-container w3-padding-32" id="test">
-    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Test Title</h3>
+    <!-- <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Test Title</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
       occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
       laboris nisi ut aliquip ex ea commodo consequat.
@@ -39,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
       occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
       laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
+    </p> -->
 
     
   </div>
--- application/views/headerNfooter/footer.php
+++ application/views/headerNfooter/footer.php
@@ -1,7 +1,16 @@
 <!-- Footer -->
 <footer class="w3-center w3-black w3-padding-16">
-  <p>Created by Code Seekers</p>
+  <!-- <p>Created by Code Seekers</p> -->
 </footer>
+<!-- Script Source Files -->
 
+	<!-- jQuery -->
+	<script src="../application/assets/js/jquery-3.5.1.min.js"></script>
+	<!-- Bootstrap 4.5 JS -->
+	<script src="../application/assets/js/bootstrap.min.js"></script>
+	<!-- Popper JS -->
+	<script src="../application/assets/js/popper.min.js"></script>
+	<!-- Font Awesome -->
+	<script src="../application/assets/js/all.min.js"></script>
 </body>
 </html>
