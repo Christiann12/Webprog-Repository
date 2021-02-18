a72ac3f960ca985cf67d5e79a1b0c5e0e7f63f56
Christiann12 <64347274+Christiann12@users.noreply.github.com>
2021-02-18 10:58:48 +0800

    feat: Created navbar functionality
    Added navbar functionality to swtich tabs. Added .htacess file to remove the index.php for the address bar and edited the base url for the website. I also fixed how styles are referenced in the link rel found in header.php and scripts found in footer.php

--- application/views/headerNfooter/footer.php
+++ application/views/headerNfooter/footer.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-sm-6 col-md-4 col-lg-4 left">
-				<p class="pt-3 titlePar">Navigate to:</p>
+				<p class="pt-2 titlePar">Navigate to:</p>
 				<ul class="navigation pt-3">
 					<li><a href="" class="navFooter">Home</a></li>
 					<li><a href="" class="navFooter">FAQs</a></li>
@@ -15,7 +15,7 @@
 				</ul>
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-4 mid">
-				<p class="pt-3 titlePar">Follow us:</p>
+				<p class="pt-2 titlePar">Follow us:</p>
 				<ul class="social pt-3">
 					<li><a href="" target="_blank"><i class="fab fa-facebook"></i></a></li>
 					<li><a href="" target="_blank"><i class="fab fa-twitter"></i></a></li>
@@ -23,7 +23,7 @@
 				</ul>
 			</div>
 			<div class="col-12 col-sm-12 col-md-4 col-lg-4 right">
-				<p class="pt-3 titlePar">Contact us:</p>
+				<p class="pt-2 titlePar">Contact us:</p>
 				<ul class="contact pt-3">
 					<li><i class="fas fa-envelope icon"></i><p class="contactp align-middle pl-2">companyname@domain.com</p></li>
 					<li><i class="fas fa-phone icon"></i><p class="contactp align-middle pl-2">09xx-xxx-xxxx</p></li>
@@ -35,12 +35,12 @@
 <!-- Script Source Files -->
 
 	<!-- jQuery -->
-	<script src="../application/assets/js/jquery-3.5.1.min.js"></script>
+	<script src="<?php echo base_url(); ?>application/assets/js/jquery-3.5.1.min.js"></script>
 	<!-- Bootstrap 4.5 JS -->
-	<script src="../application/assets/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>application/assets/js/bootstrap.min.js"></script>
 	<!-- Popper JS -->
-	<script src="../application/assets/js/popper.min.js"></script>
+	<script src="<?php echo base_url(); ?>application/assets/js/popper.min.js"></script>
 	<!-- Font Awesome -->
-	<script src="../application/assets/js/all.min.js"></script>
+	<script src="<?php echo base_url(); ?>application/assets/js/all.min.js"></script>
 </body>
 </html>
