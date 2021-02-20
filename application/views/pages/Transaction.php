eb490e222139d1bf6db7b0cc295c864af961a1d7
Gaby Valbuena <70302114+gaby-v@users.noreply.github.com>
2021-02-20 12:26:29 +0800

    feat: added avail services section
    Added 'How to Avail our Services' section in Transaction Page.

--- application/views/pages/Transaction.php
+++ application/views/pages/Transaction.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Transaction Page</title>
-	</head>
-	<body>
-		<h1>Welcome to the Transaction Page</h1>
-		<p>Put content here<p>
-	</body>
-</html>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!-- Page content -->
+<div>
+	<!-- Avail Services Section -->
+	<div class="availservices container my-5 py-5">
+		<div class="col-12">
+			<h1 class="mb-3">How to Avail our Services</h1>
+		</div>
+		<div class="infographic col-12">
+			<img src="<?php echo base_url(); ?>application/assets/images/Infographic.jpg" alt="Infographic" class="w-100">
+		</div>
+	</div>
+</div>
