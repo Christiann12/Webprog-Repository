6ecf203c21ee24499dbb21f92e8c1d9157cdd8df
Christiann12 <64347274+Christiann12@users.noreply.github.com>
2021-02-21 17:58:52 +0800

    feat: created last 3 sections for Homepage
    Created Our services section, Meet the team section, And Financial Assistance Preview section. I also added the functionality for the footer navigation section

--- application/views/pages/HomePage.php
+++ application/views/pages/HomePage.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div>
 
     <!-- About Section -->
-    <div class="aboutus">
+    <div class="aboutus card py-5 px-3 px-lg-5" >
         <div class="row">
             <div class="col-lg-5">
                 <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="5000">
@@ -14,15 +14,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         
                         <!-- Carousel content-->
                         <div class="carousel-item active">
-                            <img src="<?php echo base_url(); ?>application/assets/images/Chrysanthemum.jpg" alt="" class="w-100 h-100">
+                            <img src="<?php echo base_url(); ?>application/assets/images/Chrysanthemum.jpg" alt="Carousel Item" class="w-100 h-100">
                         </div>
 
                         <div class="carousel-item">
-                            <img src="<?php echo base_url(); ?>application/assets/images/Tulips.jpg" alt="" class="w-100 h-100">
+                            <img src="<?php echo base_url(); ?>application/assets/images/Tulips.jpg" alt="Carousel Item" class="w-100 h-100">
                         </div>
 
                         <div class="carousel-item">
-                            <img src="<?php echo base_url(); ?>application/assets/images/Desert.jpg" alt="" class="w-100 h-100">
+                            <img src="<?php echo base_url(); ?>application/assets/images/Desert.jpg" alt="Carousel Item" class="w-100 h-100">
                         </div>
 
                         <!-- Next and Previous button -->
@@ -38,8 +38,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </div>
                 </div>
             </div>
-            <div class="col-lg-7 aboutusdetails pt-3 pt-sm-3 pt-md-3 pt-lg-0" >
-                <h1 class="pb-3">
+            <div class="col-lg-7 aboutusdetails pt-3 pt-sm-3 pt-md-3 pt-lg-0 px-0 px-lg-3 px-md-3">
+                <h1 class="pb-3 text-center text-sm-center text-md-center text-lg-left">
                     ODMS Enterprise
                 </h1>
                 <P class="text-justify">
@@ -60,6 +60,144 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
     <!-- End of About us Section -->
+
+    <!-- Services section -->
+
+    
+    <div class="serviceshome container card py-5 px-3 px-lg-5 shadow">
+        <h1 class="text-center">
+            Our Services
+        </h1>
+        <div class="row">
+        
+            <div class="col-12 col-md-6 col-lg-6 serviceitem">
+                <img src="<?php echo base_url(); ?>application/assets/images/service1.png" alt="Service" class="w-100 h-100">
+                <h4 class="text-center pt-4">
+                    Service Title
+                </h4>
+                <P class="text-justify pt-0 pt-md-0 pb-5 px-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nullam faucibus laoreet sapien. Etiam luctus justo finibus ipsum lacinia, 
+                facilisis blandit erat viverra. Quisque porta odio non mattis porttitor. 
+                Fusce finibus accumsan massa, bibendum malesuada nisi. 
+                Sed at neque in risus interdum malesuada. Cras et laoreet ex, sed imperdiet diam.
+                 Cras posuere finibus est ac mollis. Praesent lacus nisl, suscipit et maximus in, 
+                 vehicula vitae ante. Mauris non ipsum ac leo sodales pretium. Phasellus feugiat nisl urna, 
+                 non cursus mi ultrices ut.
+                </P>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-6 serviceitem">
+                <img src="<?php echo base_url(); ?>application/assets/images/service2.png" alt="Service" class="w-100 h-100">
+                <h4 class="text-center pt-4">
+                    Service Title
+                </h4>
+                <P class="text-justify pt-0 pt-md-0 pb-5 px-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nullam faucibus laoreet sapien. Etiam luctus justo finibus ipsum lacinia, 
+                facilisis blandit erat viverra. Quisque porta odio non mattis porttitor. 
+                Fusce finibus accumsan massa, bibendum malesuada nisi. 
+                Sed at neque in risus interdum malesuada. Cras et laoreet ex, sed imperdiet diam.
+                 Cras posuere finibus est ac mollis. Praesent lacus nisl, suscipit et maximus in, 
+                 vehicula vitae ante. Mauris non ipsum ac leo sodales pretium. Phasellus feugiat nisl urna, 
+                 non cursus mi ultrices ut.
+                </P>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-6 serviceitem">
+                <img src="<?php echo base_url(); ?>application/assets/images/service1.png" alt="Service" class="w-100 h-100">
+                <h4 class="text-center pt-4">
+                    Service Title
+                </h4>
+                <P class="text-justify pt-0 pt-md-0 pb-5 px-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nullam faucibus laoreet sapien. Etiam luctus justo finibus ipsum lacinia, 
+                facilisis blandit erat viverra. Quisque porta odio non mattis porttitor. 
+                Fusce finibus accumsan massa, bibendum malesuada nisi. 
+                Sed at neque in risus interdum malesuada. Cras et laoreet ex, sed imperdiet diam.
+                 Cras posuere finibus est ac mollis. Praesent lacus nisl, suscipit et maximus in, 
+                 vehicula vitae ante. Mauris non ipsum ac leo sodales pretium. Phasellus feugiat nisl urna, 
+                 non cursus mi ultrices ut.
+                </P>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-6 serviceitem">
+                <img src="<?php echo base_url(); ?>application/assets/images/service2.png" alt="Service" class="w-100 h-100">
+                <h4 class="text-center pt-4">
+                    Service Title
+                </h4>
+                <P class="text-justify pt-0 pt-md-0 pb-5 px-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nullam faucibus laoreet sapien. Etiam luctus justo finibus ipsum lacinia, 
+                facilisis blandit erat viverra. Quisque porta odio non mattis porttitor. 
+                Fusce finibus accumsan massa, bibendum malesuada nisi. 
+                Sed at neque in risus interdum malesuada. Cras et laoreet ex, sed imperdiet diam.
+                 Cras posuere finibus est ac mollis. Praesent lacus nisl, suscipit et maximus in, 
+                 vehicula vitae ante. Mauris non ipsum ac leo sodales pretium. Phasellus feugiat nisl urna, 
+                 non cursus mi ultrices ut.
+                </P>
+            </div>  
+        </div>
+    </div>
+
+    <!-- End of Services Section -->
+
+    <!-- Meet our team Section -->
+        <div class="container teamsection">
+            <h1 class="mb-3 mb-md-5">
+                Meet the team
+            </h1>
+            <div class="row">
+                <div class="col-12 col-md-4 teamitem">
+                    <img src="<?php echo base_url(); ?>application/assets/images/profile1.png" alt="Service" class="w-100 rounded">
+                    <h4 class="text-center pt-4">
+                        Full Name
+                    </h4>
+                    <P class="text-justify pt-3 pt-md-0 px-3">
+                        Here goes a description or the profile of the person (name, role, quote, contact information)
+                    </P>
+                </div> 
+                <div class="col-12 col-md-4 teamitem">
+                    <img src="<?php echo base_url(); ?>application/assets/images/profile2.png" alt="Service" class="w-100 rounded">
+                    <h4 class="text-center pt-4">
+                        Full Name
+                    </h4>
+                    <P class="text-justify pt-3 pt-md-0 px-3">
+                        Here goes a description or the profile of the person (name, role, quote, contact information)
+                    </P>
+                </div> 
+                <div class="col-12 col-md-4 teamitem">
+                    <img src="<?php echo base_url(); ?>application/assets/images/profile3.png" alt="Service" class="w-100 rounded">
+                    <h4 class="text-center pt-4">
+                        Full Name
+                    </h4>
+                    <P class="text-justify pt-3 pt-md-0 px-3">
+                        Here goes a description or the profile of the person (name, role, quote, contact information)
+                    </P>
+                </div> 
+                
+            </div>
+        </div>
+    <!-- End of Meet our team Section -->
+
+    <!-- Financial Assistance preview-->
+        <div class="container finpreview card px-0">
+            <div class="row">
+                <div class="col-12 col-lg-7 px-3 py-3 px-md-5 py-md-5 px-lg-5 py-lg-5 finpreviewitem test">
+                    <h1 class="text-sm-center text-md-center text-lg-left">
+                        Financial Assistance
+                    </h1>
+                    <p class="text-justify">
+                        This is a short description of what the financial assistance feature is. By clicking the 'Go To Page' button, it will direct you to the financial assistance page that will show a complete detail of what it is.
+                    </p>
+                    <button type="button" class="btn btn-lg">Go To Page</button>
+                </div>
+                <div class="col-12 col-lg-5 px-0 mx-0 finpreviewitem" style="background-color:red;">
+                    <img src="<?php echo base_url(); ?>application/assets/images/financialpreview.png" alt="Service" class="w-100 h-100 border-rounded">
+                </div>
+            </div>
+        </div>        
+    <!-- End of Financial Assistance Preview -->
 
     <!-- style="background-color:red;" dont remove I use this for debugging -->
 </div>
