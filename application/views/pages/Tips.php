776cc8adbee51390f80686303e10e7672483b7da
Gaby Valbuena <70302114+gaby-v@users.noreply.github.com>
2021-02-23 21:44:05 +0800

    feat: added content in tips page
    Added "Disaster Preparedness", "Disaster Response", and "Disaster Recovery" sections in Tips Page.

--- application/views/pages/Tips.php
+++ application/views/pages/Tips.php
@@ -1,9 +1,99 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-echo "Tips Page";
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-</body>
-</html>
+
+<!-- Page content -->
+<div>
+    <!-- Tips Heading -->
+    <div class="tipsheading container my-5">
+        <div class="col-12">
+            <h1>Tips Page</h1>
+        </div>
+        <div class="col-12">
+            <p>A short description of what this page is, the content, etc.</p>
+        </div>
+    </div>
+
+    <!-- Disaster Preparedness -->
+    <div class="preparedness container card my-5 py-5 px-3 px-lg-5 shadow">
+        <div class="col-12">
+            <h2>Disaster Preparedness</h2>
+        </div>
+        <div class="row">
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-disaster.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-disaster.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-disaster.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Disaster Response -->
+    <div class="response container card my-5 py-5 px-3 px-lg-5 shadow">
+        <div class="col-12">
+            <h2>Disaster Response</h2>
+        </div>
+        <div class="row">
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-response.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-response.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-response.jpg" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Disaster Recovery -->
+    <div class="recovery container card my-5 py-5 px-3 px-lg-5 shadow">
+        <div class="col-12">
+            <h2>Disaster Recovery</h2>
+        </div>
+        <div class="row">
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-recovery.png" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-recovery.png" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+            <div class="infographic col-sm-4 mt-4">
+                <div>
+                    <img src="<?php echo base_url(); ?>application/assets/images/info-recovery.png" alt="Infographic" class="w-100 h-100">
+                    <p class="my-3">Source: (link)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- End page content -->
