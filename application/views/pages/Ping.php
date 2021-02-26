f23fa84ae4212d4b8c0152accf5f7e468b5e7be8
Aian Mark Antonio <aoantonio@student.apc.edu.ph>
2021-02-26 09:54:21 +0800

    feat:Create Ping Section
    Added css on header,  edited ping.php and customized/added ping.css

---
+++ application/views/pages/Ping.php
@@ -0,0 +1,60 @@
+<div>
+    <div class="ping card my-5 card py-5 px-3 px-lg-5">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>What is Ping?</h1>
+                    <h5>Lorem ipsum dolor sit amet, quo nostro detracto ne, ad minimum cotidieque eum, eu vel oratio inciderint mediocritatem. Sea te tibique intellegebat, illum copiosae accusata qui ad, ceteros dissentiunt eos in. Id assum oratio sapientem vel. Eos erant insolens contentiones ne.</h5>
+                </div>
+                <div class="col">
+                    <img src="<?php echo base_url(); ?>application/assets/images/ping_image.jpg" alt="Ping">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pinghow container my-5 py-5 px-3 px-lg-5 shadow">
+        <div class="col-12">
+            <h1>How does it works?</h1>
+        </div>
+        <div class="row">
+            <div class="column-item col-6 first-column">
+                <div>
+                    <h2>Step 1</h2>
+                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                </div>
+            </div>
+
+            <div class="column-item col-6 second-column">
+                <div>
+                    <h2>Step 2</h2>
+                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                </div>
+            </div>
+
+            <div class="column-item col-6 third-column">
+                <div>
+                    <h2>Step 3</h2>
+                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                </div>
+            </div>
+
+            <div class="column-item col-6 fourth-column">
+                <div>
+                    <h2>Step 4</h2>
+                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pingimgbtn card my-5 py-5 px-3 px-lg-5">
+            <div class="imgbtn col-sm-12 mt-12">
+                <img src="<?php echo base_url(); ?>application/assets/images/tower_ping.jpg" alt="Tower" class="img-fluid" class="h-100 w-100">
+                <div>
+                    <h1>Start Pinging Now</h1>
+                    <button class="img-btn btn-primary">Ping</button>
+                </div>
+            </div>
+    </div>
+</div>
