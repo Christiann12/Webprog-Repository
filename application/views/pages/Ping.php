f24afb6df704ea9844c2688a73a0eef8ec7c07b1
Aian Mark Antonio <aoantonio@student.apc.edu.ph>
2021-02-28 12:37:24 +0800

    feat: Created modal
    Added modal, modified the steps description in pinghow container and added some css for modals

--- application/views/pages/Ping.php
+++ application/views/pages/Ping.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col">
                     <h1>What is Ping?</h1>
-                    <h5>Lorem ipsum dolor sit amet, quo nostro detracto ne, ad minimum cotidieque eum, eu vel oratio inciderint mediocritatem. Sea te tibique intellegebat, illum copiosae accusata qui ad, ceteros dissentiunt eos in. Id assum oratio sapientem vel. Eos erant insolens contentiones ne.</h5>
+                    <h5>This section helps the users of the website quickly alert ODMS in time of disaster or after a disaster. This will send ODMS the information they need for quick response.</h5>
                 </div>
                 <div class="col">
                     <img src="<?php echo base_url(); ?>application/assets/images/ping_image.jpg" alt="Ping">
@@ -21,28 +21,28 @@
             <div class="column-item col-6 first-column">
                 <div>
                     <h2>Step 1</h2>
-                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                    <h5>Start by clicking the ping button located below the page.</h5>
                 </div>
             </div>
 
             <div class="column-item col-6 second-column">
                 <div>
                     <h2>Step 2</h2>
-                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                    <h5>Input your company's name and contact information.</h5>
                 </div>
             </div>
 
             <div class="column-item col-6 third-column">
                 <div>
                     <h2>Step 3</h2>
-                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                    <h5>Wait for a confirmation reply.</h5>
                 </div>
             </div>
 
             <div class="column-item col-6 fourth-column">
                 <div>
                     <h2>Step 4</h2>
-                    <h5>Lorem ipsum dolor sit amet, decore suscipit sea ex, sit ad harum maiorum, est adhuc complectitur et. Ad saepe accusata est.</h5>
+                    <h5>Start communicating.</h5>
                 </div>
             </div>
         </div>
@@ -53,8 +53,38 @@
                 <img src="<?php echo base_url(); ?>application/assets/images/tower_ping.jpg" alt="Tower" class="img-fluid" class="h-100 w-100">
                 <div>
                     <h1>Start Pinging Now</h1>
-                    <button class="img-btn btn-primary">Ping</button>
-                </div>
-            </div>
+                    <button class="img-btn btn-primary" data-toggle="modal" data-target="#modalContent">Ping</button>
+
+                    <div class="modal fade" id="modalContent" tabindex="-1" aria-labelledby="modalContentLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalContentLabel">Ping Information</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="company-name" class="company"><h6 class="company">Company Name:</h6></label>
+                                                <input type="text" class="form-control" id="company-name" placeholder="Ex: ODMS Enterprise">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="email-info" class="email"><h6 class="email">Contact Info:</h6></label>
+                                                <input type="text" class="form-control" id="contact-email" placeholder="Ex: Email or Telephone Number">
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="button" class="cancel-btn btn btn-default" data-dismiss="modal">Quit</button>
+                                        <button type="button" class="proceed-btn btn btn-default" >Ping</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        </div>
     </div>
 </div>
