30e1de52d215c1e178f1d67efc3f63c5abee829e
MjBasco <77535077+MjBasco@users.noreply.github.com>
2021-03-01 00:53:26 +0800

    Edited: Financial Assistance PHP and CSS file
    - Added link to Financial Assistance
      CSS file in header PHP file.
    - Added media query to CSS file
    - Fixed Margins and columns of
      Financial Assistance page.
    - Fixed errors/mistakes in homepage
      (finpreview button color, finpreview
      size.).
    - Changed company1.jpeg to
       company2.png.

--- application/views/pages/FinancialAssistance.php
+++ application/views/pages/FinancialAssistance.php
@@ -7,48 +7,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		Our Client Companies
 	</h1>
 </div>
+
+
+<!-- Company Preview -->
 	<div class="container companyPreview card px-0">
     	<div class="row">
-        	<div class="col-6 px-0 mx-0 companyPreviewitem" style="background-color:red;">
-            	<img src="<?php echo base_url(); ?>application/assets/images/company1.jpeg" alt="Company1" class="w-100 h-100 border-rounded">
+        	<div class="col-12 col-sm-12 col-md-12 col-lg-5 px-0 mx-0 companyPreviewitem" style="background-color:red;">
+            	<img src="<?php echo base_url(); ?>application/assets/images/company1.png" alt="Company1" class="w-100 h-100 border-rounded">
        		</div>
-       	    <div class="col-6 px-3 py-3 px-md-5 py-md-5 px-lg-5 py-lg-5 companyPreviewitem test">
-            	<h1 class="text-sm-center text-md-center text-lg-left my-5">
+       	    <div class="col-12 col-sm-12 col-md-12 col-lg-7 px-3 py-3 px-md-5 py-md-5 px-lg-5 py-lg-5 companyPreviewitem test">
+            	<h1 style="color: #18191a">
                		Company Name
             	</h1>
-            	<p class="text-justify">
+            	<p class="text-justify py-5" style="color: #18191a">
                 	The requirements/needs of the company goes here. 
             	</p>
             	<div class="row">
-            		<button type="button" class="btn1 btn-lg my-sm-0 my-md-0 my-lg-0" onclick="window.location.href='<?php echo base_url(); ?>main/index/####'">Website</button>
-            		<button type="button" class="btn2  btn-lg"  onclick="window.location.href='<?php echo base_url(); ?>main/index/Transaction'">Go To Transactions</button>
+            		<button type="button" class="FAbtn1" onclick="window.location.href='<?php echo base_url(); ?>main/index/####'"> Website</button>
+            		<button type="button" class="FAbtn2"  onclick="window.location.href='<?php echo base_url(); ?>main/index/Transaction'">Go To Transactions
+                    </button>
             	</div>
             </div>
         	</div>
     	</div>
-	</div> 
-	<div class="container companyPreview2 card px-0">
-    	<div class="row">
-        	<div class="col-6 px-0 mx-0 companyPreviewitem2" style="background-color:red;">
-            	<img src="<?php echo base_url(); ?>application/assets/images/company2.png" alt="Company1" class="w-100 h-100 border-rounded">
-       		</div>
-       	    <div class="col-6 px-3 py-3 px-md-5 py-md-5 px-lg-5 py-lg-5 companyPreviewitem2 test">
-            	<h1 class="text-sm-center text-md-center text-lg-left my-5">
-               		Company Name
-            	</h1>
-            	<p class="text-justify">
-                	The requirements/needs of the company goes here. 
-            	</p>
-            	<div class="row">
-            		<button type="button" class="btn1 btn-lg my-sm-0 my-md-0 my-lg-0" onclick="window.location.href='<?php echo base_url(); ?>main/index/####'">
-            		Website
-            		</button>
+	</div>
 
-            		<button type="button" class="btn2 btn-lg " onclick="window.location.href='<?php echo base_url(); ?>main/index/Transactions'">
-            		Go To Transactions
-            		</button>
-            	</div>
+
+    <div class="container companyPreview card px-0" style="background-color: #18191a">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-5 px-0 mx-0 companyPreviewitem" style="background-color:red;">
+                <img src="<?php echo base_url(); ?>application/assets/images/company2.png" alt="Company2" class="w-100 h-100 border-rounded">
             </div>
-        	</div>
-    	</div>
-	</div>               
+            <div class="col-12 col-sm-12 col-md-12 col-lg-7 px-3 py-3 px-md-5 py-md-5 px-lg-5 py-lg-5 companyPreviewitem test">
+                <h1 style="color: #FFEA00">
+                    Company Name
+                </h1>
+                <p class="text-justify py-5"  style="color: #FFEA00">
+                    The requirements/needs of the company goes here. 
+                </p>
+                <div class="row">
+                    <button type="button" class="FAbtn1" onclick="window.location.href='<?php echo base_url(); ?>main/index/####'"> Website</button>
+                    <button type="button" class="FAbtn2"  onclick="window.location.href='<?php echo base_url(); ?>main/index/Transaction'">Go To Transactions
+                    </button>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+<!-- End of Company Preview -->
