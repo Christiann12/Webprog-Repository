776eb08b065ca18771cf294e978f5aaf8bdb79c7
Christiann12 <64347274+Christiann12@users.noreply.github.com>
2021-03-02 15:00:14 +0800

    feat: Added Admin Name
    Added admin name in the navigation tab in the admin page and made transaction page tables dynamic and responds to number of clicks for transaction buttons in the main page

--- application/views/adminpages/pages/wrapper.php
+++ application/views/adminpages/pages/wrapper.php
@@ -22,20 +22,32 @@
 
    <!-- Admin Navbar -->
     <div id="page-content-wrapper">
-
+            
         <nav class="navbar navbar-expand-lg navbar-light bg-light rounded-lg mt-3">
             <button class="btn border-dark" id="menu-toggle"><i class="fas fa-bars"></i></button>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-sign-out-alt"></i></span>
             </button>
-
+            
+            
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
             <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url(); ?>admin/index/loginpage">Sign Out <span class="sr-only">(current)</span></a>
+                <li  class="nav-item">
+                    
+                    <p class="nav-link mx-0 my-0">
+                        Welcome, Ava Barnes
+                    </p>
+
                 </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link sign-out" href="<?php echo base_url(); ?>admin/index/loginpage">Sign Out <span class="sr-only">(current)</span></a>
+                </li>
+                
             </ul>
+            
             </div>
         </nav>
         <!-- Page content -->
