e059a194ef152ec92b26c2a1bcf0e8c2b6f5ff2b
MjBasco <77535077+MjBasco@users.noreply.github.com>
2021-03-02 12:47:33 +0800

    Update supportadmin.php
    Edited Columns
    A. Contact info now split into 2 columns
        1. Contact Number
        2. E-mail Address
    B. Address now split into 2 columns
       1. Street Address
       2. City Address

--- application/views/adminpages/pages/supportadmin.php
+++ application/views/adminpages/pages/supportadmin.php
@@ -15,8 +15,10 @@
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
                     <th scope="col">Company Name</th>
-                    <th scope="col">Contact Info</th>
-                    <th scope="col">Address</th>
+                    <th scope="col">Contact Number</th>
+                    <th scope="col">E-mail Address</th>
+                    <th scope="col">Street Address</th>
+                    <th scope="col">City Address</th>
                     <th scope="col">Subject</th>
                 </tr>
             </thead>
@@ -25,64 +27,80 @@
                     <th scope="row">1</th>
                     <td>Eren Jaeger</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>11 Zeke St., Paradis Village, Manila</td>
+                    <td>11 Zeke St.</td>
+                    <td>Paradis City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
                     <td>Mikasa Ackerman</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>26 Braun St., Paradis Village, Manila</td>
+                    <td>26 Braun St.</td>
+                    <td>Paradis City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
                     <td>Armin Arlert</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>13 Erwin St., Paradis Village, Manila</td>
+                    <td>13 Erwin St.</td>
+                    <td>Paradis City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">4</th>
                     <td>Sasha Braus</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>12 Jean St., Paradis Village, Manila</td>
+                    <td>12 Jean St.</td>
+                    <td>Paradis City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">5</th>
                     <td>Annie Leonhart</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>22 Hoover St., Marley Village, Manila</td>
+                    <td>22 Hoover St.</td>
+                    <td>Marley City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">6</th>
                     <td>Colt Grice</td>
                     <td>Company</td>
+                    <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>28 Porco St., Marley Village, Manila</td>
+                    <td>28 Porco St.</td>
+                    <td>Marley City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">7</th>
                     <td>Levi Ackerman</td>
                     <td>Company</td>
+                     <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>19 Petra St., Paradis Village, Manila</td>
+                    <td>19 Petra St.</td>
+                    <td>Paradis City</td>
                     <td>Subject</td>
                 </tr>
                 <tr>
                     <th scope="row">8</th>
                     <td>Connie Springer</td>
                     <td>Company</td>
+                     <td>####-###-####</td>
                     <td>company@domain.com</td>
-                    <td>21 Historia St., Paradis Village, Manila</td>
+                    <td>21 Historia St.</td>
+                    <td>Paradis CIty</td>
                     <td>Subject</td>
                 </tr>
             </tbody>
